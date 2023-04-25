49b33dcaddea80508b26b0e9c869a157cae1bd32
Katherine928 <katherine71317@gmail.com>
2023-04-25 15:46:38 -0400

    Graphql Union and Caching

---
+++ src/main/java/net/katherine/graphqlplayground/lec11/config/TypeResolverConfig.java
@@ -0,0 +1,26 @@
+package net.katherine.graphqlplayground.lec11.config;
+
+
+import graphql.schema.TypeResolver;
+import net.katherine.graphqlplayground.lec11.dto.FruitDto;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.execution.ClassNameTypeResolver;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+
+@Configuration
+public class TypeResolverConfig {
+
+    @Bean
+    public RuntimeWiringConfigurer configurer(TypeResolver resolver){
+        return c -> c.type("Result", b -> b.typeResolver(resolver));
+    }
+
+    @Bean
+    public TypeResolver typeResolver(){
+        ClassNameTypeResolver resolver = new ClassNameTypeResolver();
+        resolver.addMapping(FruitDto.class, "Fruit");
+        return resolver;
+    }
+
+}
---
+++ src/main/java/net/katherine/graphqlplayground/lec11/dto/Book.java
@@ -0,0 +1,15 @@
+package net.katherine.graphqlplayground.lec11.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor(staticName = "create")
+public class Book {
+
+    private String title;
+    private String author;
+
+}
